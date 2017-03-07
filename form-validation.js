89562cfbaf28fae91865b6e3e51dcd1e4e625777
Unknown <pbegbie@mail.csuchico.edu>
2017-03-07 08:30:58 -0800

    Added necessary condition statements and got the following to work

--- form-validation.js
+++ form-validation.js
@@ -4,16 +4,27 @@ function validate(){
   var passEntered = document.getElementById("pass").value;
 
   //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
+  var sln = userEntered.length;
+  if (userEntered.length >= 6){
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  if (userEntered.length < 6){
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
 
   //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+  if (passEntered == "password"){
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  } else {
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
+
 }
